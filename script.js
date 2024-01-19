1afb07e74b23e3ffe6438bdcf4dd3519164ce1ea
cemsagiroglu <156402266+cemsagiroglu@users.noreply.github.com>
2024-01-18 20:38:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctPassword = "python"; // Replace with your desired password
+const correctPassword = "test"; // Replace with your desired password
 
 function checkPassword() {
   const enteredPassword = document.getElementById("password").value;
